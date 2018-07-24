a563924948f4ba8858a76d11fff61b07c04b0b3b
yarplux <yarplux@yandex.ru>
2018-07-24 17:24:39 +0300

    Задача #2.2
    Функционал:
    
     + макет формы в выбранных цветах

--- app/src/main/java/com/shifu/user/recrutin_android/MainActivity.java
+++ app/src/main/java/com/shifu/user/recrutin_android/MainActivity.java
@@ -1,13 +1,43 @@
 package com.shifu.user.recrutin_android;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationHost{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new GridFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * Navigate to the given fragment.
+     *
+     * @param fragment       Fragment to navigate to.
+     * @param addToBackstack Whether or not the current fragment should be added to the backstack.
+     */
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
     }
 }
