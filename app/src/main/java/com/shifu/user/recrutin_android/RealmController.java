3a7556c7a8b88d47f82fbf0b2d39aa340821c8d1
yarplux <yarplux@yandex.ru>
2018-07-27 19:02:17 +0300

    + убраны уже не рабочие и не использующиеся тестовые функции/классы/ресурсы + изменена формат выдачи - отображает зарплату, если есть, дату загрузки вакансии (пока в изначальном формате)

--- app/src/main/java/com/shifu/user/recrutin_android/RealmController.java
+++ app/src/main/java/com/shifu/user/recrutin_android/RealmController.java
@@ -1,21 +1,16 @@
 package com.shifu.user.recrutin_android;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
 import com.shifu.user.recrutin_android.json.Job;
 import com.shifu.user.recrutin_android.json.JoobleJsonResponse;
-import com.shifu.user.recrutin_android.json.JsonFake;
 import com.shifu.user.recrutin_android.json.JsonJob;
 import com.shifu.user.recrutin_android.realm.Jobs;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,7 +31,6 @@ public class RealmController {
                 .build();
 
         realm = Realm.getInstance(config);
-        //realm = Realm.getDefaultInstance();
     }
 
 
@@ -47,67 +41,40 @@ public class RealmController {
 
     public void addJobs(final List<JsonJob> data, final Handler h) {
         final String TAG = CLASS_TAG+"addJobs";
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(@NotNull Realm realm) {
-                realm.where(Jobs.class).findAll().deleteAllFromRealm();
-                for (JsonJob obj : data) {
-                    Jobs item = realm.createObject(Jobs.class, UUID.randomUUID().toString());
-                    item.setTitle(obj.getTitle());
-                    item.setCompany(obj.getCompany());
-                    item.setDescription(obj.getDescription());
-                    item.setSalary(Long.toString(obj.getSalary()));
-                    item.setUrl(obj.getUrl());
-                }
-
-                //MainActivity.setRA(new RealmRVAdapter(realm.where(Jobs.class).findAll().sort("title")));
-                //MainActivity.getRA().notifyDataSetChanged();
-                h.sendMessage(Message.obtain(h, 1, TAG));
+        realm.executeTransactionAsync(realm -> {
+            realm.where(Jobs.class).findAll().deleteAllFromRealm();
+            for (JsonJob obj : data) {
+                Jobs item = realm.createObject(Jobs.class, UUID.randomUUID().toString());
+                item.setTitle(obj.getTitle());
+                item.setCompany(obj.getCompany());
+                item.setDescription(obj.getDescription());
+                item.setSalary(Long.toString(obj.getSalary()));
+                item.setUrl(obj.getUrl());
             }
+
+            //MainActivity.setRA(new RealmRVAdapter(realm.where(Jobs.class).findAll().sort("title")));
+            //MainActivity.getRA().notifyDataSetChanged();
+            h.sendMessage(Message.obtain(h, 1, TAG));
         });
     }
 
     public void addJoobleJobs(final JoobleJsonResponse data, final Handler h) {
         final String TAG = CLASS_TAG+"addJoobleJobs";
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(@NotNull Realm realm) {
+        realm.executeTransactionAsync(realm -> {
 
-                for (Job obj : data.getJobs()) {
-                    Jobs item = realm.createObject(Jobs.class, UUID.randomUUID().toString());
-                    item.setTitle(obj.getTitle());
-                    item.setCompany(obj.getCompany());
-                    item.setDescription(obj.getSnippet());
-                    item.setSalary(obj.getSalary());
-                    item.setUrl(obj.getLink());
-                }
-                Log.d(TAG, "Loaded : "+realm.where(Jobs.class).count());
-                h.sendMessage(Message.obtain(h, 1, TAG));
+            for (Job obj : data.getJobs()) {
+                Jobs item = realm.createObject(Jobs.class, UUID.randomUUID().toString());
+                item.setTitle(obj.getTitle());
+                item.setCompany(obj.getCompany());
+                item.setDescription(obj.getSnippet());
+                item.setSalary(obj.getSalary());
+                item.setUrl(obj.getLink());
+                item.setUpdated(obj.getUpdated());
             }
+            Log.d(TAG, "Loaded : "+realm.where(Jobs.class).count());
+            h.sendMessage(Message.obtain(h, 1, TAG));
         });
     }
-
-
-    public void setData(final List<JsonFake> data, final Handler h) {
-        final String TAG = CLASS_TAG+"setData";
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(@NotNull Realm realm) {
-                realm.where(Jobs.class).findAll().deleteAllFromRealm();
-                for (JsonFake obj : data) {
-                    Jobs item = realm.createObject(Jobs.class, UUID.randomUUID().toString());
-                    item.setTitle(obj.title);
-                    item.setCompany(obj.company);
-                    item.setDescription(obj.description);
-                    item.setSalary(obj.salary);
-                    item.setUrl(obj.url);
-                }
-
-                h.sendMessage(Message.obtain(h, 1, TAG));
-            }
-        });
-    }
-
 
     /**
      * READ DATA FUNCTIONS _________________________________________________________________________
@@ -118,37 +85,8 @@ public class RealmController {
         return realm.where(objClass).count();
     }
 
-    public List<JsonFake> getData(String sortField, Handler h){
-        RealmResults<Jobs> base;
-        final String TAG = CLASS_TAG+"getData";
-        boolean sort = false;
-        if (sortField != null)
-        {
-            for (Field f: Jobs.class.getDeclaredFields()) {
-                if (f.getName().equals(sortField)){
-                    sort = true;
-                    break;
-                }
-            }
-        }
-
-        if (sort) {
-            base = realm.where(Jobs.class).findAll().sort(sortField);
-        } else {
-            base = realm.where(Jobs.class).findAll();
-        }
-
-        List<JsonFake> list=new ArrayList <>();
-        for (Jobs obj : base) {
-            list.add(new JsonFake(obj.getTitle(), obj.getDescription(), obj.getSalary(), obj.getCompany(), obj.getUrl()));
-        }
-        h.sendMessage(Message.obtain(h, 1, TAG));
-        return list;
-    }
-
     public <T extends RealmObject> RealmResults<T> getBase(Class<T> objClass, String sortField){
         RealmResults<T> base;
-        final String TAG = "RC.getBase";
         boolean sort = false;
         if (sortField != null)
         {
@@ -205,34 +143,28 @@ public class RealmController {
      */
 
     public void clear (final Handler h) {
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(@NotNull Realm realm) {
-                realm.where(Jobs.class).findAll().deleteAllFromRealm();
-                h.sendMessage(Message.obtain(h, 1, "RC.clear"));
+        realm.executeTransactionAsync(realm -> {
+            realm.where(Jobs.class).findAll().deleteAllFromRealm();
+            h.sendMessage(Message.obtain(h, 1, "RC.clear"));
 
-            }
         });
     }
 
     public void removeItemById(final String id, final Handler h) {
         final String TAG = "RC.removeItemById";
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(@NotNull Realm realm) {
-                try {
-                    Jobs item = realm.where(Jobs.class).equalTo((String)Jobs.class.getField("FIELD_ID").get(null), id).findFirst();
-                    if (item != null) {
-                        item.deleteFromRealm();
-                        MainActivity.getRA().notifyDataSetChanged();
-                    }
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();
-                    h.sendMessage(Message.obtain(h, 0, TAG));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                    h.sendMessage(Message.obtain(h, 0, TAG));
+        realm.executeTransactionAsync(realm -> {
+            try {
+                Jobs item = realm.where(Jobs.class).equalTo((String)Jobs.class.getField("FIELD_ID").get(null), id).findFirst();
+                if (item != null) {
+                    item.deleteFromRealm();
+                    MainActivity.getRA().notifyDataSetChanged();
                 }
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+                h.sendMessage(Message.obtain(h, 0, TAG));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+                h.sendMessage(Message.obtain(h, 0, TAG));
             }
         });
     }
